18e21a1d21df13dab7b86d256077cada91f8233f
sheth <shethr22@gmail.com>
2025-03-03 10:26:54 -0700

    created all the folders and files

---
+++ basics/payroll_jenkinclass/TEST/main.py
@@ -0,0 +1,3 @@
+# main.py is the main file that will be executed when the application is run. It will import the app object from the app module and run it.
+# new file and add the following code:
+# # flask app for payroll endpoint
---
+++ basics/payroll_jenkinclass/TEST/readme.md
@@ -0,0 +1,7 @@
+# write cdode for readme.md what is meaning of md
+hello 
+def main();
+''' 
+this is the main function to start flask app
+'''
+
